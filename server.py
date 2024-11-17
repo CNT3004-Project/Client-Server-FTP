9cb48e61c02512ff7890eb31bc896ffee633d350
DevJake123 <89547864+DevJake123@users.noreply.github.com>
2024-11-17 02:14:30 -0500

    Implemented Multithreading, Also added more information to be said in the console from the server to help find errors

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 import os
+import threading
 
 IP = socket.gethostbyname(socket.gethostname())
 PORT = 4455
@@ -36,16 +37,17 @@ def handle_client(conn, addr):
             elif file_ext in [".mp4", ".mkv", ".avi"] and file_size > 2*1024*1024*1024:
                 conn.send("[ERROR] Video files must be at most 2GB, please split the video files up".encode(FORMAT))
             else:
+                print(f"[INFO] Starting file upload: {filename} with size {file_size} bytes.")
                 with open(file_path, "wb") as file:
                     bytes_received = 0
                     while bytes_received < file_size:
                         chunk = conn.recv(SIZE)
                         if not chunk:
+                            print("[ERROR] No chunk received.")
                             break
                         file.write(chunk)
                         bytes_received += len(chunk)
-
-                print(f"[INFO] Received {bytes_received}/{file_size} bytes.")
+                        print(f"[INFO] Received {bytes_received}/{file_size} bytes.")
 
                 if bytes_received == file_size:
                     print(f"[INFO] File {filename} uploaded successfully.")
@@ -53,6 +55,8 @@ def handle_client(conn, addr):
                 else:
                     print("[ERROR] File upload incomplete.")
                     conn.send("[ERROR] File upload failed.".encode(FORMAT))
+
+
         #sends requested file to client
         elif command == "DOWNLOAD":
             filename = args[0]
@@ -91,7 +95,8 @@ def main():
 
     while True:
         conn, addr = server.accept() #accepts connections
-        handle_client(conn, addr)
+        client_thread = threading.Thread(target=handle_client, args=(conn,addr))
+        client_thread.start()
 
 
 if __name__ == "__main__":
